3666860e8a728cb4da05b32449b35333bc7a149e
JimiKhosravi <113926772+JimiKhosravi@users.noreply.github.com>
2023-01-17 16:51:25 +0100

    Added Alert.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from "./Alert";
 
 class NumberOfEvents extends Component {
-  state = { noe: 10 }
+  state = {
+    noe: 10,
+    errorText: ""
+  }
 
   componentDidMount() {
     this.setState({ noe: this.props.noe || 10 });
@@ -10,12 +14,16 @@ class NumberOfEvents extends Component {
   changeNOE(value) {
     this.setState({ noe: value });
     this.props.updateNumberOfEvents(value);
+    if (value < 1 || value > 32) {
+      this.setState({ errorText: "Select number from 1 to 32" });
+    } else this.setState({ errorText: "" });
   }
 
   render() {
-    const { noe } = this.state;
+    const { noe, errorText } = this.state;
     return (
       <div className="NumberOfEvents">
+        <ErrorAlert text={errorText} />
         <h3>Number of Events:</h3>
         <input
           className="noe-input"
