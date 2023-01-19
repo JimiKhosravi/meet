00e82484dcadac0225dcebeb1e736193eec4ed22
JimiKhosravi <113926772+JimiKhosravi@users.noreply.github.com>
2023-01-19 17:54:15 +0100

    Updated

--- src/api.js
+++ src/api.js
@@ -53,10 +53,10 @@ export const getEvents = async () => {
     return mockData;
   }
 
-  if (!navigator.onLine) {
+  if (!navigator.online) {
     const data = localStorage.getItem("lastEvents");
     NProgress.done();
-    return data ? JSON.parse(events).events : [];;
+    return data ? JSON.parse(data).events : [];
   }
 
   const token = await getAccessToken();
